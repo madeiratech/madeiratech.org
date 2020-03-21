06ab9d70e9ac40e3a170750a37696155a7893d75
Hugo Romano <hugoromano@gmail.com>
2020-03-21 01:45:28 +0000

    version and cache control

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import _ from "lodash";
 import mime from "mime";
 import { DateTime } from "luxon";
+import { version } from "../package.json";
 
 /**
  * Respond to the request
  * @param {Request} request
  */
 const handleRequest = async event => {
+  const cache_control = "max-age=600";
   return new Response(
     `madeira tech meetup
 sponsored by: cloudflare and cowork funchal`,
-    { status: 200, headers: { "Content-Type": mime.getType("txt") } }
+    {
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "x-version": version,
+        "content-type": mime.getType("txt"),
+        "cache-control": cache_control
+      }
+    }
   );
 };
 
